27ba11ee497850a165102a7a394043269f2eebca
jesusmh <eduardoomh@outlook.com>
2023-12-10 19:00:12 -0600

    new theme and responsive

--- components/Maintenance/Maintenance.tsx
+++ components/Maintenance/Maintenance.tsx
@@ -8,12 +8,17 @@ const Maintenance = () => {
             <br />
             <p className={styles.paragraph}>Desarrollamos soluciones de software meticulosamente <br />hechas para ti.</p>
             <br />
+            <p className={styles.emoji_container}>
+                <img src='/images/emoji1.png' width={36} height={36} alt='emoji_01' />
+                <img src='/images/emoji2.png' width={36} height={36} alt='emoji_02' />
+            </p>
+            <br/>
             <section className={styles.img_container}>
                 <img src='/images/dev.png' width={450} height={450} alt="dev-in-computer" />
             </section>
             <br />
             <p className={styles.second_paragraph}>
-                Actualmente nos encontramos en mantenimiento, <br /> vuelve pronto.
+                Actualmente nos encontramos en  <br />mantenimiento, vuelve pronto.
             </p>
         </article>
     )
